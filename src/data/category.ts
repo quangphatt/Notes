fe6a0157e76ed3c14d03c8ca1c05ea159b025cd7
quangphatt <phatquang2811@gmail.com>
2026-03-06 11:19:53 +0700

    Define Categories

---
+++ src/data/category.ts
@@ -0,0 +1,15 @@
+import {
+  BookOpenTextIcon,
+  HeartPulseIcon,
+  DumbbellIcon,
+} from 'lucide-react-native';
+
+export const CATEGORIES = [
+  { key: 'work_and_study', name: 'Work and study', icon: BookOpenTextIcon },
+  { key: 'life', name: 'Life', icon: HeartPulseIcon },
+  {
+    key: 'health_and_wellness',
+    name: 'Health and wellness',
+    icon: DumbbellIcon,
+  },
+];
