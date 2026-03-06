f98d4197df8bcce1d6a368a1b2eef3dc9dc58ce8
quangphatt <phatquang2811@gmail.com>
2026-03-06 10:45:34 +0700

    Config theme

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,6 @@ module.exports = {
   extends: '@react-native',
   rules: {
     '@typescript-eslint/no-unused-vars': 'off',
+    'react-native/no-inline-styles': 'off',
   },
 };
---
+++ src/theme/color.ts
@@ -0,0 +1,8 @@
+export const COLORS = {
+  primary: '#FF1493',
+  background: '#2A1546',
+  surface: '#3D2561',
+  text: '#FFFFFF',
+  muted: '#999999',
+  dark: '#220B3A',
+};
